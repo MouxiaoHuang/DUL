cf10d9a5016cf38e768cf4112ec4589bef37f72a
Mouxiao Huang <neohuang469@gmail.com>
2021-09-12 17:36:27 +0800

    Update TestFR_webface_DUL.sh

--- exp/TestFR_webface_DUL.sh
+++ exp/TestFR_webface_DUL.sh
@@ -2,7 +2,7 @@ export CUDA_VISIBLE_DEVICES=3
 
 logs_test_file='./logs_test/testfr_webface_dul.log'
 
-model_for_test='./exp/checkpoints/exp_webface_dul/Backbone_IR_SE_64_DUL_Epoch_22_Batch_19558_Time_2021-09-11-01-39_checkpoint.pth'
+model_for_test=''
 
 python ../test_fr_dul.py \
     --model_for_test $model_for_test \
