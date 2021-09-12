8f66bba6bc5c13efa8b3574fdda518eb7be90c5b
Mouxiao Huang <neohuang469@gmail.com>
2021-09-12 17:36:10 +0800

    Create TestFR_ms1m_DUL.sh

---
+++ exp/TestFR_ms1m_DUL.sh
@@ -0,0 +1,9 @@
+export CUDA_VISIBLE_DEVICES=3
+
+logs_test_file='./logs_test/testfr_ms1m_dul.log'
+
+model_for_test=''
+
+python ../test_fr_dul.py \
+    --model_for_test $model_for_test \
+    >> $logs_test_file 2>&1 &
